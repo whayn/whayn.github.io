b73cdc1367f83ab3ca5cd45b94087c49161cca2f
whayn <mayeul.falgayrettes@gmail.com>
2021-09-27 11:23:14 +0200

    Google analytics + comments on index (to do for other files too)

---
+++ _analytics-manager-noscript.js
@@ -0,0 +1,7 @@
+document.write(`
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK94QP4"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+`)
+document.getElementById("analytics-manager-noscript-script").remove()
